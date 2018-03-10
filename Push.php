eec415f32fe0aca43544d890b1e241ab195d6e81
mohit-agarwal18 <m.agarwal2018@gmail.com>
2018-03-10 18:13:05 +0530

    PHP FIREBASE NOTIFICATION

---
+++ Push.php
@@ -0,0 +1,64 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+
+
+
+
+
+class Push extends CI_Controller {
+
+
+
+function __construct()
+
+
+
+	{
+		parent::__construct();
+
+}
+
+
+public function index(){
+
+
+
+#API access key from Google API's Console
+    define( 'API_ACCESS_KEY', 'AAAAVDV3f1I:APA91bGh7JzeaozsKXwqYUSW8FAsj14pzEKgIJOVpBwWUXE_HVweHhmeHIpWiaexYwPAa2dhtNOKPGiIMiuEF8KhvI2EC1zs1gijYYOF8ofQfqNbZksai6PN0D_n6dgEwBM49d94dSXe' );
+    $registrationIds = $_GET['id'];
+#prep the bundle
+     $msg = array
+          (
+		'body' 	=> 'PHP',
+		'title'	=> 'php',
+             	'icon'	=> 'myicon',/*Default Icon*/
+              	'sound' => 'mySound'/*Default sound*/
+          );
+	$fields = array
+			(
+				'to'		=> $registrationIds,
+				'notification'	=> $msg
+			);
+	
+	
+	$headers = array
+			(
+				'Authorization: key=' . API_ACCESS_KEY,
+				'Content-Type: application/json'
+			);
+#Send Reponse To FireBase Server	
+		$ch = curl_init();
+		curl_setopt( $ch,CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send' );
+		curl_setopt( $ch,CURLOPT_POST, true );
+		curl_setopt( $ch,CURLOPT_HTTPHEADER, $headers );
+		curl_setopt( $ch,CURLOPT_RETURNTRANSFER, true );
+		curl_setopt( $ch,CURLOPT_SSL_VERIFYPEER, false );
+		curl_setopt( $ch,CURLOPT_POSTFIELDS, json_encode( $fields ) );
+		$result = curl_exec($ch );
+		curl_close( $ch );
+#Echo Result Of FireBase Server
+echo $result;
+}
+
+}
